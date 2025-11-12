35dae368202587137455e2810c0f9084ce011121
Matthew Russo <mdrusso@mit.edu>
2025-11-11 23:57:17 -0500

    add security group for ssh

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -16,9 +16,15 @@ variable "subnet_id" {
   default     = "subnet-27021a61"
 }
 
+variable "vpc_id" {
+  description = "The VPC ID where the EC2 instance will be launched."
+  type        = string
+  default     = "vpc-d27da3b7"
+}
+
 variable "deployer_public_key" {
   description = "The public SSH key for the deployer."
   type        = string
   default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKLX7sunnyaSl6i2JCO3fkSLU51lSwZShWCX8p5dbnGQ"
-  
+
 }
