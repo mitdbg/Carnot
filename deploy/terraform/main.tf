dd27306f265e43515d8a0aadb8f568316364b875
Matthew Russo <mdrusso@mit.edu>
2025-11-17 11:28:31 -0500

    update to using nginx and port 80

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -173,20 +173,20 @@ resource "aws_lb_listener" "http_redirect" {
 # create target group for EC2 instance
 resource "aws_lb_target_group" "app_tg" {
   name     = "app-tg"
-  port     = 5173
+  port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
 
   health_check {
     path = "/"
-    port = "5173"
+    port = "80"
   }
 }
 
 resource "aws_lb_target_group_attachment" "app_attachment" {
   target_group_arn = aws_lb_target_group.app_tg.arn
   target_id        = aws_instance.app_server.id
-  port             = 5173
+  port             = 80
 }
 
 # create A record in Route53 to point to ALB
