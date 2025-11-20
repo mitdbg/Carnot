dd7bfc5be16bd0ca1f4f2dda34611a15052cfdc3
zhuohangu <zhuohan@uchicago.edu>
2025-11-20 14:03:56 -0600

    feat: add prepare_quest_documents to auto-retrieval module exports

--- src/carnot/core/data/auto_retrieval/__init__.py
+++ src/carnot/core/data/auto_retrieval/__init__.py
@@ -2,5 +2,6 @@
 
 from .client import SearchClient
 from .types import Query, SearchResult, SearchError
+from .quest_data_prep import prepare_quest_documents
 
-__all__ = ["SearchClient", "Query", "SearchResult", "SearchError"]
+__all__ = ["SearchClient", "Query", "SearchResult", "SearchError", "prepare_quest_documents"]
