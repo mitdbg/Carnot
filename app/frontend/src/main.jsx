c5df144ab67d48e69a5a7a2bdd31afd9bed8f146
Matthew Russo <mdrusso@mit.edu>
2025-11-20 23:44:22 -0500

    fix bug

--- app/frontend/src/main.jsx
+++ app/frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import { Auth0Provider } from '@auth0/auth0-react';
@@ -19,4 +20,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </Auth0Provider>
   </React.StrictMode>,
 )
-
