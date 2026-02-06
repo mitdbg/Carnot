0d56d4891e01ef289c739c7fbab3a69b8827fa45
Matthew Russo <mdrusso@mit.edu>
2026-02-06 10:49:13 -0500

    update aws ami

--- deploy/terraform/env/main.tf
+++ deploy/terraform/env/main.tf
@@ -41,7 +41,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-20250115"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
 
   owners = ["099720109477"] # Canonical
